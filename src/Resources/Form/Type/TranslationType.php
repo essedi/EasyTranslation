ccd5468f275b280cffb43d4a22035023f47d0a09
dani <dani@debian>
2018-06-22 12:53:04 +0200

    change namespace

--- src/Resources/Form/Type/TranslationType.php
+++ src/Resources/Form/Type/TranslationType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Essedi\EasyTranslation\Form\Type;
+namespace Essedi\EasyTranslation\Resources\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\LocaleType;
